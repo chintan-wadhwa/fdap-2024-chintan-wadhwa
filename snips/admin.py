f1b3306717f644f0045c3f310bd97206fe1822ae
woebbeking <woebbeking@axiomatec.com>
2024-03-20 21:48:26 +0100

    Add list_filter and search_fields to SnipAdmin

--- snips/admin.py
+++ snips/admin.py
@@ -6,9 +6,16 @@ class ClassroomAdmin(admin.ModelAdmin):
 
 class SnipsheetAdmin(admin.ModelAdmin):
     list_display = ('snipsheet_id', 'classroom_id')
+    list_filter = ('classroom_id',)
 
 class SnipAdmin(admin.ModelAdmin):
-    list_display = ('snip_id', 'student_id', 'snipsheet')
+    list_display = ('snip_id', 'student_id', 'snipsheet', 'get_classroom_id')
+    search_fields = ('student_id', 'snip_id')
+    list_filter = ('snipsheet',)
+
+    def get_classroom_id(self, obj):
+        return obj.snipsheet.classroom.classroom_id
+    get_classroom_id.short_description = 'Classroom ID'  # Sets column name
 
 admin.site.register(Classroom, ClassroomAdmin)
 admin.site.register(Snipsheet, SnipsheetAdmin)
