bcea6542cd6a1e2e96e54abcc075e6a35ee8c81f
Fabian Woebbeking <fwg@iwh-halle.de>
2024-06-04 21:07:31 +0200

    Update DEBUG setting to True for local development

--- studysnips/settings.py
+++ studysnips/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xfgy4ofmu9$(j))5a^ijx$_6bgbo@5&8ywn!+3nkr6h-zglq1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "studysnips.eu.pythonanywhere.com"]
 
